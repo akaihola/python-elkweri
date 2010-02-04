e540ff058cf04a7837c7616368b0341b6c66bdd0
Antti Kaihola <akaihol+python@ambitone.com>
2010-02-04 16:54:31 +0200

    Added a setup.py and tagged version 0.01

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = 'elkweri',
+    version = '0.01',
+    packages = ['elkweri'],
+    author = 'Antti Kaihola',
+    author_email = 'akaihol+python@ambitone.com',
+    description = ('DSL for testing HTML DOMs'),
+    url = 'http://github.com/akaihola/python-elkweri',
+    download_url = ('http://www.github.com/akaihola/python-elkweri/'
+                    'tarball/0.01'),
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development'),
+    )
